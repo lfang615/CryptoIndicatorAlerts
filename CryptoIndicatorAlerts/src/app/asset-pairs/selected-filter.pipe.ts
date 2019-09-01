1d6ed26a9e858f8e4c492b50d0be896f85041d47
Larry Fang <lfang615@gmail.com>
2019-08-31 19:39:48 -0500

    filter pipe cleanup

--- CryptoIndicatorAlerts/src/app/asset-pairs/selected-filter.pipe.ts
+++ CryptoIndicatorAlerts/src/app/asset-pairs/selected-filter.pipe.ts
@@ -5,12 +5,6 @@ import { AssetPair } from './asset-pair.model';
 })
 export class SelectedFilterPipe implements PipeTransform {
   transform(items: AssetPair[]): any[] {
-    //if (!items) return [];
-    //if (!searchText) return items;
-    //searchText = searchText.toLowerCase();
-    //return items.filter(it => {
-    //  return it.baseName.toLowerCase().includes(searchText);
-    //});
     return items.filter(x => x.IsSelected);
   }
 }
